ff21cd4a213a1d52b91702522ad537d3962a3c4a
cprovelengios <cprovelengios@gmail.com>
2021-04-27 12:49:44 +0300

    Fix test_model.py

--- test_model.py
+++ test_model.py
@@ -5,33 +5,22 @@ from tensorflow.keras.models import load_model
 
 if __name__ == '__main__':
     steering_sensitivity = 1    # Maybe 0.7 ??
-    model = load_model('model.h5')
+    model = load_model('Models/model_v0.h5')
 
     path = 'Training_Data'
     data = import_data_info(path=path, start_folder=0, end_folder=1)
 
-    data = balance_data(data, display=False)
     images_path, steerings = load_data(path, data)
+    test_images = 20
 
-    index = random.randint(0, len(images_path) - 1)
-    img = cv2.imread(images_path[index])
-    img = pre_process(img)
-    img = np.array([img])
+    for i in range(test_images):
+        index = np.random.randint(len(images_path))
+        img = cv2.imread(images_path[index])
+        img = pre_process(img)
+        img = np.array([img])
 
-    steering = float(model.predict(img))
-    print(steering * steering_sensitivity)
+        steering = float(model.predict(img))
+        print(steering * steering_sensitivity)
 
-    cv2.imshow('Test Image', cv2.imread(images_path[index]))
-    cv2.waitKey(0)
-
-    # path = 'Training_Data/IMG0/Image_1619189861989188.jpg'
-    #
-    # img = cv2.imread(path)
-    # img = pre_process(img)
-    # img = np.array([img])
-    #
-    # steering = float(model.predict(img))
-    # print(steering * steering_sen)
-    #
-    # cv2.imshow('Test Image', cv2.imread(path))
-    # cv2.waitKey(0)
+        cv2.imshow('Test Image', cv2.imread(images_path[index]))
+        cv2.waitKey(0)
--- try.py
+++ try.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3.7
+import random
+import numpy as np
+
+
+a = [1, 2]
+
+print(np.random.randint(len(a)))
+print(random.randint(0, len(a) - 1))
+
