be2d978ba4366c4adb21f592675e624a1652bd0c
cprovelengios <cprovelengios@gmail.com>
2021-04-27 19:28:13 +0300

    Fix camera resolution

--- camera.py
+++ camera.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3.7
 import cv2
 
-# Camera Resolution: 1920*1080 (which is 1080p) creates an image that is 1920 pixels wide and 1080 pixels tall.
 cap = cv2.VideoCapture(0)
-cap.set(3, 1920)
-cap.set(4, 1080)
+cap.set(3, 640)     # frame width
+cap.set(4, 360)     # frame height
 
 
-def get_img(display=False, width=640, height=480):
+def get_img(display=False, width=640, height=360):
     ret, img = cap.read()
     img = cv2.resize(img, (width, height))
 
